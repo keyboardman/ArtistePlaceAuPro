d25a9218e836f9f087498d199add6138b1a5948e
Retr0LoveLace <benjy93600@gmail.com>
2024-06-02 12:33:03 +0200

    maj

---
+++ src/Controller/UserController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\UserProfileType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class UserController extends AbstractController
+{
+
+    #[Route('/user/settings', name: 'app_settings')]
+    public function index(Request $request, EntityManagerInterface $em): Response
+    {
+
+        $form = $this->createForm(UserProfileType::class, $this->getUser());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            $plainPassword = $form->get("plainPassword")->getData();
+            if (!empty($plainPassword)){
+                $hashed = password_hash($plainPassword, PASSWORD_DEFAULT);
+                $user->setPassword($hashed);
+            }
+
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', 'Your profile has been updated.');
+
+            return $this->redirectToRoute('app_settings');
+        }
+
+        return $this->render('settings/index.html.twig', [
+            'controller_name' => 'SettingsController',
+            'form' => $form->createView(),
+        ]);
+    }
+}
